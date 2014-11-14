50090aff7326796fdcf5c0ac521d3579e749d8d9
stefanius <KIT_kat_16>
2014-11-14 20:33:51 +0100

    Removed GenerateFixturesBundle and added to its own repo

--- sites/brigittavalentijn/public_html/src/Stef/GenerateFixturesBundle/Generator/TypeGeneratorInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Stef\GenerateFixturesBundle\Generator;
-
-
-interface TypeGeneratorInterface {
-
-    public function execute();
-} 
--- sites/brigittavalentijn/public_html/src/Stef/GenerateFixturesBundle/StefGenerateFixturesBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Stef\GenerateFixturesBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class StefGenerateFixturesBundle extends Bundle
-{
-}
